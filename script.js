0e31ae6fd7fa5994494b9b81755c9df8a0a72d9a
jigen3971 <jigen3971@gmail.com>
2026-05-16 23:47:24 +0900

    changed

--- script.js
+++ script.js
@@ -6,6 +6,27 @@ const message = document.getElementById("message");
 
 rentButton.addEventListener("click", () => {
 
+  if (
+    !document.querySelector('input[placeholder="氏名"]').value ||
+    !document.querySelector('input[placeholder="TEL"]').value ||
+    !document.querySelector('input[placeholder="預かり車 車種"]').value ||
+    !document.querySelector('input[placeholder="預かり車 ナンバー"]').value ||
+    !document.getElementById("staff").value ||
+    !document.getElementById("sign").value
+  ){
+    alert("必須項目を入力してください");
+    return;
+  }
+
+  const checks = document.querySelectorAll('.check input[type="checkbox"]');
+
+  for (let check of checks) {
+    if (!check.checked) {
+      alert("確認事項をすべてチェックしてください");
+      return;
+    }
+  }
+
   const data = {
     rentalCar: document.getElementById("rentalCar").value,
     name: document.querySelector('input[placeholder="氏名"]').value,
@@ -36,21 +57,6 @@ rentButton.addEventListener("click", () => {
 });
 
 returnButton.addEventListener("click", () => {
-    rentButton.addEventListener("click", () => {
-
-  if (
-    !document.querySelector('input[placeholder="氏名"]').value ||
-    !document.querySelector('input[placeholder="TEL"]').value ||
-    !document.querySelector('input[placeholder="預かり車 車種"]').value ||
-    !document.querySelector('input[placeholder="預かり車 ナンバー"]').value ||
-    !document.getElementById("staff").value ||
-    !document.getElementById("sign").value
-  ){
-    alert("必須項目を入力してください");
-    return;
-  }
-
-  const data = {
 
   if (returnButton.dataset.done === "true") {
     return;
