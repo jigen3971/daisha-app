f63333351f45aadbfee9d4910508c7f8eb75824b
jigen3971 <jigen3971@gmail.com>
2026-05-16 21:25:42 +0900

    変更

--- script.js
+++ script.js
@@ -22,11 +22,10 @@ rentButton.addEventListener("click", () => {
     returnCheck: ""
   };
 
-  fetch("https://script.google.com/macros/s/AKfycbxAE7CZbHKhe02W2WTQF4NQxtNsL7w8yr6rb2t7ueyLtRoXbiSoYJRDxCZLweaoTrHc/exec", {
+fetch("https://script.google.com/macros/s/AKfycbxAE7CZbHKhe02W2WTQF4NQxtNsL7w8yr6rb2t7ueyLtRoXbiSoYJRDxCZLweaoTrHc/exec", {
   method: "POST",
   body: new URLSearchParams(data)
 });
-
   message.innerHTML = "貸出確認をスプレッドシートへ保存しました";
   message.style.color = "green";
 
